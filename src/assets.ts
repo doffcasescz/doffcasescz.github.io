05958960779e003575ee2d6d9f05ed32b103ebfb
Philipp Egorov <philavr@digitalandy.eu>
2024-06-04 21:57:46 +0000

    chore: Refactor asset renaming and indexing logic

--- src/assets.ts
+++ src/assets.ts
@@ -1,16 +1,59 @@
-
-import {glob} from "fast-glob";
-import {rename} from "fs/promises";
-import {join} from "path";
+import { glob } from "fast-glob"
+import { rename, writeFile } from "fs/promises"
+import { join } from "path"
 
 async function renameFolders(path: string, oldName: string, newName: string) {
-    const folders = await glob(join(path, `**/${oldName}`), {onlyDirectories: true});
-    await Promise.all(folders.map(folder => rename(folder, folder.replace(oldName, newName))));
+  const folders = await glob(join(path, `**/${oldName}`), { onlyDirectories: true })
+  await Promise.all(folders.map((folder) => rename(folder, folder.replace(oldName, newName))))
+}
+
+async function assetsIndex() {
+    const deviceIndex = await glob("assets/device/*", {deep: 0, onlyDirectories: true})
+  const deviceFolderNames = deviceIndex.map((path) => path.split("/").pop())
+  console.log("deviceFolderNames", deviceFolderNames)
+  // console.log("deviceIndex", deviceIndex)
+
+  let assetIndex = []
+
+  for (const device of deviceFolderNames) {
+    const blackAssets = await glob(`assets/device/${device}/black/*`, {onlyFiles: true})
+    const beigeAssets = await glob(`assets/device/${device}/beige/*`, {onlyFiles: true})
+
+    assetIndex.push({
+        device: device,
+        camera: blackAssets.find((path) => path.includes("Camera")),
+        black: {
+          bg: blackAssets.find((path) => path.includes("BG")),
+          frame: blackAssets.find((path) => path.includes("Frame")),
+        },
+        beige: {
+          bg: beigeAssets.find((path) => path.includes("BG")),
+          frame: beigeAssets.find((path) => path.includes("Frame")),
+        }
+      })
+
+    // assetIndex[device] = {
+    // camera: blackAssets.find((path) => path.includes("Camera")),
+    //   black: {
+    //     bg: blackAssets.find((path) => path.includes("BG")),
+    //     frame: blackAssets.find((path) => path.includes("Frame")),
+    //   },
+    //   beige: {
+    //     bg: beigeAssets.find((path) => path.includes("BG")),
+    //     frame: beigeAssets.find((path) => path.includes("Frame")),
+    //   }
+    // }
+  }
+
+  return assetIndex
 }
 
 // await renameFolders("assets/device", "biege", "beige");
+// delete Camera.png from beige folder && move Camera.png up to device folder
 
-(async () => {
-    
-    // rest of your code...
+
+;(async () => {
+    const deviceIndex = await (await assetsIndex()).sort((a, b) => a.device.localeCompare(b.device))
+    console.log("deviceIndex", deviceIndex)
+    writeFile("assets/device.json", JSON.stringify(deviceIndex, null, 2))
 })()
